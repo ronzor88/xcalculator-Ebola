bb1b57eb654ffcc6589d6bd66c56862a7269c6f8
ronzor88 <ronzor88@gmail.com>
2014-10-01 13:01:22 +0200

    klarprodukt av kalkylator

--- kalkylator.py
+++ kalkylator.py
@@ -1,89 +1,109 @@
 # -*- coding: cp1252 -*-
 print "Välkommen till vår grymma kalkylator!"
+running = True
+while running:
+    def plus (a, b):
+        return a+b
+    def minus (a, b):
+        return a-b
+    def multi (a, b):
+        return a*b
+    def div (a, b):
+        return a/b
 
-def plus (a, b):
-    return a+b
-def minus (a, b):
-    return a-b
-def multi (a, b):
-    return a*b
-def div (a, b):
-    return a/b
 
+    def medeltal (a):
+        sum =0
+        for i in a:
+            sum = plus(sum, i);
 
-def medeltal (a):
-    sum =0
-    for i in a:
-        sum = plus(sum, i);
+        return div(sum, len(a))
 
-    return div(sum, len(a))
+    def median(lst):
+        sortedLst = sorted(lst)
+        lstLen = len(lst)
+        index = (lstLen - 1) // 2
 
-def median(lst):
-    sortedLst = sorted(lst)
-    lstLen = len(lst)
-    index = (lstLen - 1) // 2
+        if (lstLen % 2):
+            return sortedLst[index]
+        else:
+            return (sortedLst[index] + sortedLst[index + 1])/2.0
+    def summa (a):
+        sum =0
+        for i in a:
+            sum = plus(sum, i);
 
-    if (lstLen % 2):
-        return sortedLst[index]
-    else:
-        return (sortedLst[index] + sortedLst[index + 1])/2.0
-def summa (a):
-    sum =0
-    for i in a:
-        sum = plus(sum, i);
+        return sum
 
-    return sum
-
-def upp (a,b):
-    return a**b
-print "Välj vilken funktion du vill använda:"
-print "\n (1) Plus \n (2) Minus \n (3) Multiplicera \n (4) Dividera"
-print "\n (5) Medeltal \n (6) Median \n (7) Summa flera tal \n (8) Upphöjning"
-operation = input("Välj funktion (1-8): ")
-if operation == 1:
-    a = input("Tal 1: ")
-    b = input("Tal 2: ")
-    print plus(a, b)
-if operation == 2:
-    a = input("Tal 1: ")
-    b = input("Tal 2: ")
-    print minus (a, b)
-if operation == 3:
-    a = input("Tal 1: ")
-    b = input("Tal 2: ")
-    print multi(a, b)
-if operation == 4:
-    a = input("Tal 1: ")
-    b = input("Tal 2: ")
-    print div(a, b)
-if operation == 5:
-    inputLista = []
-    inputting = True
-    while inputting:
-        inputtal = raw_input("Lägg till ett tal, X när du är klar: ")
-        if inputtal == "x" or inputtal == "X":
-            inputting = False
-            continue
-        inputLista.append(int(inputtal))
-        print "Just nu har du: "
-        print inputLista
-    
-    print medeltal(inputLista)
-if operation == 6:
-    inputLista = []
-    inputting = True
-    while inputting:
-        inputtal = raw_input("Lägg till ett tal, X när du är klar: ")
-        if inputtal == "x" or inputtal == "X":
-            inputting = False
-            continue
-        inputLista.append(int(inputtal))
-        print "Just nu har du: "
-        print inputLista
-    
-    print median(inputLista)
+    def upp (a,b):
+        return a**b
+    print "Välj vilken funktion du vill använda:"
+    print "\n (1) Plus \n (2) Minus \n (3) Multiplicera \n (4) Dividera"
+    print "\n (5) Medeltal \n (6) Median \n (7) Summa flera tal \n (8) Upphöjning"
+    operation = input("Välj funktion (1-8): ")
+    if operation == 1:
+        a = input("Tal 1: ")
+        b = input("Tal 2: ")
+        print plus(a, b)
+    if operation == 2:
+        a = input("Tal 1: ")
+        b = input("Tal 2: ")
+        print minus (a, b)
+    if operation == 3:
+        a = input("Tal 1: ")
+        b = input("Tal 2: ")
+        print multi(a, b)
+    if operation == 4:
+        a = input("Tal 1: ")
+        b = input("Tal 2: ")
+        print div(a, b)
+    if operation == 5:
+        inputLista = []
+        inputting = True
+        while inputting:
+            inputtal = raw_input("Lägg till ett tal, X när du är klar: ")
+            if inputtal == "x" or inputtal == "X":
+                inputting = False
+                continue
+            inputLista.append(int(inputtal))
+            print "Just nu har du: "
+            print inputLista
+        
+        print medeltal(inputLista)
+    if operation == 6:
+        inputLista = []
+        inputting = True
+        while inputting:
+            inputtal = raw_input("Lägg till ett tal, X när du är klar: ")
+            if inputtal == "x" or inputtal == "X":
+                inputting = False
+                continue
+            inputLista.append(int(inputtal))
+            print "Just nu har du: "
+            print inputLista
+        
+        print median(inputLista)
+    if operation == 7:
+        inputLista = []
+        inputting = True
+        while inputting:
+            inputtal = raw_input("Lägg till ett tal, X när du är klar: ")
+            if inputtal == "x" or inputtal == "X":
+                inputting = False
+                continue
+            inputLista.append(int(inputtal))
+            print "Just nu har du: "
+            print inputLista
+        
+        print summa(inputLista)
+    if operation == 8:
+        a = input("Tal 1: ")
+        b = input("Upphöjt till: ")
+        print upp(a, b)
+            
 
         
-
-    
-
+    runAgain = raw_input("Vill du räkna igen?(y/n): ")
+    if runAgain == "n":
+        running = False
+    continue
