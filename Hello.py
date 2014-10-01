0b0470f2a750672fdd048117cd4115e3a9ffb528
Fackoz <marcus.agerup@gmail.com>
2014-10-01 12:58:10 +0200

    Marcus kod

---
+++ Hello.py
@@ -0,0 +1,20 @@
+def plus(x, y):
+        return x + y
+
+def minus(x, y):
+        return x - y
+
+def divsion(x, y):
+        return x / y
+    
+def multiplikation(x, y):
+        return x * y
+
+def medeltal(x):
+    sum=0
+    for i in x:
+        sum += i
+    return sum/len(x)
+
+def raised(x, y):
+    return x**y
